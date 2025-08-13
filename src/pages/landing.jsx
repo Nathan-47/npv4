909302a4f10c7fcf8da0eb1d8cdf736670dde3a6
Nathan-47 <nathan.parchment47@gmail.com>
2025-08-14 00:04:01 +0100

    landing buttons and styling update

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { TbFileCv } from "react-icons/tb";
 
 
 const Landing = () => {
@@ -10,6 +13,53 @@ const Landing = () => {
           <p id="hero-job" className="mt-6 text-lg leading-8 text-gray-300">
           Currently a Web Developer @ <a className="a-link" href="https://www.bcusu.com/">BCUSU</a>
           </p>
+
+
+        {/* about section */}
+        {/* TODO: Add about heading here */}
+        <p id="content-intro">Hi ✋🏾 I'm a web developer currently working at BCUSU, An amazing independent charity, that gives students' their best experience at University. My main focus is to build and design accessible, smooth and slick student digital experiences. 
+        <br />
+        Away from the desk, I am reading a book or playing games such as Football Manager, God of War and FIFA. I am also a food lover that hikes, powerlifts and plays footie on the side</p>
+
+
+      {/* history section */}
+      {/* TODO: Add experience heading here */}
+      <div className='grid grid-cols-3 gap-4 text-gray-300' id="job-text">
+        <div className='job-desc'>2021 - PRESENT</div>
+         <div className="col-span-2">
+          <p className='job-title'>Web Developer | BCUSU</p>
+          <br />
+          <p className='job-desc'>Working with Graphics/UI Designer to Develop and design more accessible, informative and smoother navigation to students at Birmingham City University.</p>
+          </div>
+
+          <div className='job-desc'>2025 - PRESENT</div>
+         <div className="col-span-2">
+          <p className='job-title'>Accessibility Developer | CWW Mind</p>
+          <br />
+          <p className='job-desc'>Using tools such as Skilltide and Lighthouse to help increase WCAG accessibility scores across the website.</p>
+          </div>
+      </div>
+
+      <div className="contact-icon">
+      <a href=''><TbFileCv />View Resume</a>
+      </div>
+
+      {/* contact section */}
+      {/* TODO: Add contact heading here */}
+      <div className="grid grid-cols-3 gap-4">
+      <div className="contact-icon">
+        <a href='mailto:nathan.parchment47@gmail.com'><IoIosMail />Email</a>
+      </div>
+
+      <div className="contact-icon">
+        <a href='https://github.com/Nathan-47' target="_blank" rel="noopener noreferrer"><FaGithub /> Github</a>
+      </div>
+
+      <div className="contact-icon">
+        <a href='https://www.linkedin.com/in/nathanparchment/' target="_blank" rel="noopener noreferrer"><FaLinkedin />LinkedIn</a>
+      </div>
+      </div>
+
       </div>
       </div>
     )
