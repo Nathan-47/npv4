4566580f5c6700248478ebdd6ee9239809147ad4
Nathan-47 <nathan.parchment47@gmail.com>
2025-08-20 22:05:03 +0100

    ahref replaced by react-router Link

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import marvelplay from '../images/marvel_play.svg';
 
 
@@ -7,7 +8,7 @@ const projects = () => {
             <h1>Projects</h1>
             <div className="cards-wrapper">
                 <div class="grid grid-cols-3 gap-12 group">
-                    <a href="">
+                    <Link to="/marvelplay">
                         <div className="project-card transition-opacity duration-300 group-hover:opacity-40 hover:!opacity-100">
                         <img className="thumbnail" src={marvelplay} alt="" />
                         <h2 className='project-title'>Marvel Play</h2>
@@ -15,9 +16,9 @@ const projects = () => {
                         <p className="pill-2">Netlify</p>
                         <p className='project-text text-gray-300'>A Marvel character search filter using API to fetch an array of characters with a 1v1 RPG style card game also.</p>
                         </div>
-                    </a>
+                    </Link>
 
-                    <a href="">
+                    <Link to="">
                         <div className="project-card transition-opacity duration-300 group-hover:opacity-40 hover:!opacity-100">
                         <img className="thumbnail" src={marvelplay} alt="" />
                         <h2 className='project-title'>Marvel Play</h2>
@@ -25,9 +26,9 @@ const projects = () => {
                         <p className="pill-2">Netlify</p>
                         <p className='project-text text-gray-300'>A Marvel character search filter using API to fetch an array of characters with a 1v1 RPG style card game also.</p>
                         </div>
-                    </a>
+                    </Link>
 
-                    <a href="">
+                    <Link to="">
                         <div className="project-card transition-opacity duration-300 group-hover:opacity-40 hover:!opacity-100">
                         <img className="thumbnail" src={marvelplay} alt="" />
                         <h2 className='project-title'>Marvel Play</h2>
@@ -35,7 +36,7 @@ const projects = () => {
                         <p className="pill-2">Netlify</p>
                         <p className='project-text text-gray-300'>A Marvel character search filter using API to fetch an array of characters with a 1v1 RPG style card game also.</p>
                         </div>
-                    </a>
+                    </Link>
                 </div>
 
                 {/* <div class="grid grid-cols-3 !mt-20 gap-12 group">
