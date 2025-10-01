972961e3b4433d7225347bf1c4683c750621c3d6
Nathan-47 <nathan.parchment47@gmail.com>
2025-10-01 21:28:47 +0100

    learning and improvement sections added

--- src/pages/fitrquest.jsx
+++ src/pages/fitrquest.jsx
@@ -17,6 +17,28 @@ const fitrquest = () => {
                 <FaGithub  className="git-icon" /><Link to="https://github.com/Nathan-47/fitrquest" target="_blank" rel="noopener noreferrer">View Code</Link>
             </div>
 
+            <div className="learn-section">
+                <h2 className="!mb-6"><u>What I learnt</u></h2>
+                <ul className="list-disc list-inside !space-y-6 text-white">
+                    <li>Using Postman to test URL routes was a first for me and made testing login and signup endpoints easy to see if they’re working and if any errors occur that affect user experience.</li>
+
+                    <li>JWT Tokens was a first for me also and I was intrigued on how it is used in applications. As I wanted to create a page for only signed up users this was perfect as JWT verifies indentities and protect routes.</li>
+
+                    <li>Using AuthContext I learnt that it tracks authentication state across the app. Letting components check whether a user is logged in without passing props, making it an easier experience for me development wise.</li>
+
+                    <li>Building a MERN app MongoDB is imperitive and I learned how to use schemas to define structured fields like name, email, and password for login/signup.</li>
+                </ul>
+            </div>
+
+            <div className="future-section !mt-12">
+                <h2 className="!mb-6"><u>Future Improvments</u></h2>
+                <ul className="list-disc list-inside !space-y-4 text-white">
+                 <li>Regarding the schema I will look to apply rules such as required and unique to enforce data integrity and simplify management.</li>    
+                 <li>Provide more questions to get a more refined/accurate end result</li>
+                <li>Allow user to create a new password</li>
+                </ul>
+            </div>
+
             <div className="project-screenshots">
                 <a href={fitimg1} target="_blank" rel="noopener noreferrer"><img className="screenshot-thumb" src={fitimg1} alt="Fitrquest quiz start screen that begins with I am followed by two answers 15-44 or 45+" /></a>
                 <a href={fitimg2} target="_blank" rel="noopener noreferrer"><img className="screenshot-thumb" src={fitimg2} alt="Completed quiz page path that is displaying all information on Powerlifting" /></a>
