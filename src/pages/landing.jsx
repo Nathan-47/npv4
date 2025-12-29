1f3f60a09de7630522d6521beea8ce5da78935df
Nathan-47 <nathan.parchment47@gmail.com>
2025-12-29 01:19:18 +0000

    Removed second job description

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -18,8 +18,8 @@ const Landing = () => {
         {/* about section */}
         <h2>About</h2>
         <p id="content-intro">Hi ✋🏾 I'm a web developer currently working at BCUSU, An amazing independent charity, that gives students' their best experience at University. My main focus is to build and design accessible, smooth and slick student digital experiences. 
-        <br />
-        Away from the desk, I am reading a book or playing games such as Football Manager, God of War and FIFA. I am also a food lover that hikes, powerlifts and plays footie on the side</p>
+        <br /><br />
+        Away from the desk, I am reading a book or playing games such as Football Manager, God of War and FIFA. I am also a food lover that hikes, powerlift and plays footie on the side</p>
 
 
       {/* history section */}
@@ -31,18 +31,8 @@ const Landing = () => {
           <br />
           <p className='job-content'>Working with Graphics/UI Designer to Develop and design more accessible, informative and smoother navigation to students at Birmingham City University.</p>
           <span className="pill">JavaScript</span>
-          <span className="pill">CSS</span>
           <span className="pill">JQuery</span>
           <span className="pill">Figma</span>
-          </div>
-
-        <div className='job-desc'>2025 - PRESENT</div>
-         <div className="col-span-2">
-          <p className='job-title'>Volunteer Accessibility Developer | CWW Mind</p>
-          <br />
-          <p className='job-content'>Using tools such as Skilltide and Lighthouse to help increase WCAG accessibility scores across the website.</p>
-          <span className="pill">Wordpress</span>
-          <span className="pill">Skilltide</span>
           </div>
       </div>
 
